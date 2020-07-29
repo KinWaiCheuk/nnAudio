10296f4be8929e0e5ba5498831f3b45a470ab466
Tan Hao Hao <tanh0207@e.ntu.edu.sg>
2020-07-29 16:27:25 +0800

    add inverse and griffin-lim to STFT

--- Installation/nnAudio/Spectrogram.py
+++ Installation/nnAudio/Spectrogram.py
@@ -456,13 +456,6 @@ class STFT(torch.nn.Module):
         kernels will also be caluclated and the STFT kernels will be updated during model training. 
         Default value is ``False``
 
-    output_format : str
-        Determine the return type. 
-        ``Magnitude`` will return the magnitude of the STFT result, shape = ``(num_samples, freq_bins,time_steps)``; 
-        ``Complex`` will return the STFT result in complex number, shape = ``(num_samples, freq_bins,time_steps, 2)``; 
-        ``Phase`` will return the phase of the STFT reuslt, shape = ``(num_samples, freq_bins,time_steps, 2)``. 
-        The complex number is stored as ``(real, imag)`` in the last axis. Default value is 'Magnitude'. 
-
     verbose : bool
         If ``True``, it shows layer information. If ``False``, it suppresses all prints
 
@@ -484,7 +477,7 @@ class STFT(torch.nn.Module):
 
     def __init__(self, n_fft=2048, freq_bins=None, hop_length=512, window='hann', 
                 freq_scale='no', center=True, pad_mode='reflect', 
-                fmin=50, fmax=6000, sr=22050, trainable=False, output_format='Magnitude', 
+                fmin=50, fmax=6000, sr=22050, trainable=False, 
                 verbose=True, device='cuda:0'):
         
         super(STFT, self).__init__()
@@ -494,80 +487,168 @@ class STFT(torch.nn.Module):
         self.center = center
         self.pad_mode = pad_mode
         self.n_fft = n_fft
+        self.freq_bins = freq_bins
         self.trainable = trainable
-        self.output_format=output_format
         self.device = device
+        self.pad_amount = self.n_fft // 2
+        self.window = window
         start = time()
         
         # Create filter windows for stft
-        wsin, wcos, self.bins2freq, self.bin_list = create_fourier_kernels(n_fft=n_fft, 
+        wsin, wcos, self.bins2freq, self.bin_list = create_fourier_kernels(n_fft, 
                                                                            freq_bins=freq_bins, 
                                                                            window=window, 
                                                                            freq_scale=freq_scale, 
                                                                            fmin=fmin,
                                                                            fmax=fmax, 
                                                                            sr=sr)
+        # Create filter windows for inverse
+        wsin_inv, wcos_inv, _, _ = create_fourier_kernels(n_fft, 
+                                                          freq_bins=n_fft, 
+                                                          window='ones', 
+                                                          freq_scale=freq_scale, 
+                                                          fmin=fmin,
+                                                          fmax=fmax, 
+                                                          sr=sr)
+        
         self.wsin = torch.tensor(wsin, dtype=torch.float, device=self.device)
         self.wcos = torch.tensor(wcos, dtype=torch.float, device=self.device)
+        self.wsin_inv = torch.tensor(wsin_inv, dtype=torch.float, device=self.device)
+        self.wcos_inv = torch.tensor(wcos_inv, dtype=torch.float, device=self.device)
 
         # Making all these variables nn.Parameter, so that the model can be used with nn.Parallel
         self.wsin = torch.nn.Parameter(self.wsin, requires_grad=self.trainable)
         self.wcos = torch.nn.Parameter(self.wcos, requires_grad=self.trainable)
-
-        # if self.trainable==True:
-        #     self.wsin = torch.nn.Parameter(self.wsin)
-        #     self.wcos = torch.nn.Parameter(self.wcos)
+        self.wsin_inv = torch.nn.Parameter(self.wsin_inv, requires_grad=self.trainable)
+        self.wcos_inv = torch.nn.Parameter(self.wcos_inv, requires_grad=self.trainable)
 
         if verbose==True:
             print("STFT kernels created, time used = {:.4f} seconds".format(time()-start))
         else:
             pass
 
-    def forward(self,x):
+    def forward(self, x, output_format='Complex'):
+        self.output_format = output_format
+        self.num_samples = x.shape[-1]
+        
         x = broadcast_dim(x)
         if self.center:
             if self.pad_mode == 'constant':
-                padding = nn.ConstantPad1d(self.n_fft//2, 0)
+                padding = nn.ConstantPad1d(self.pad_amount, 0)
+            
             elif self.pad_mode == 'reflect':
-                padding = nn.ReflectionPad1d(self.n_fft//2)
+                if self.num_samples < self.pad_amount:
+                    raise AssertionError("Signal length shorter than reflect padding length (n_fft // 2).")
+                padding = nn.ReflectionPad1d(self.pad_amount)
 
             x = padding(x)
-            
+        
         spec_imag = conv1d(x, self.wsin, stride=self.stride) 
         spec_real = conv1d(x, self.wcos, stride=self.stride)  # Doing STFT by using conv1d
         
-        if self.output_format=='Magnitude':
+        # remove redundant parts
+        spec_real = spec_real[:, :self.freq_bins, :]
+        spec_imag = spec_imag[:, :self.freq_bins, :]
+                
+        if output_format=='Magnitude':
+            self.output_format = 'Magnitude'
             spec = spec_real.pow(2) + spec_imag.pow(2)
             if self.trainable==True:
                 return torch.sqrt(spec+1e-8)  # prevent Nan gradient when sqrt(0) due to output=0
             else:
                 return torch.sqrt(spec)
-        elif self.output_format=='Complex':
+        
+        elif output_format=='Complex':
+            self.output_format = 'Complex'
             return torch.stack((spec_real,-spec_imag), -1)  # Remember the minus sign for imaginary part
             
-        elif self.output_format=='Phase':
-            # +0.0 helps remove -0.0 elements, which leads to error in calcuating phase
-            return torch.atan2(-spec_imag+0.0,spec_real)  
-
-            # This part is for implementing the librosa.core.magphase
-            # But it seems it is not useful
-            # phase_real = torch.cos(torch.atan2(spec_imag,spec_real))
-            # phase_imag = torch.sin(torch.atan2(spec_imag,spec_real))
-            # return torch.stack((phase_real,phase_imag), -1)
+        elif output_format=='Phase':
+            self.output_format = 'Phase'
+            return torch.atan2(-spec_imag+0.0,spec_real)  # +0.0 removes -0.0 elements, which leads to error in calculating phase
     
-    def manual_forward(self,x):
-        x = broadcast_dim(x)
-        if self.center:
-            if self.pad_mode == 'constant':
-                padding = nn.ConstantPad1d(self.n_fft//2, 0)
-            elif self.pad_mode == 'reflect':
-                padding = nn.ReflectionPad1d(self.n_fft//2)
+    def inverse(self, X):
+        if len(X.shape) == 3 and self.output_format == 'Magnitude':
+            return self.griffin_lim(X)
+        elif len(X.shape) == 4 and self.output_format == "Complex":
+            return self.__inverse(X)
+        else:
+            raise AssertionError("Only perform inverse function on Magnitude or Complex spectrogram.")
+    
+    def __inverse(self, X, is_window_normalized=True):
+        X_real, X_imag = X[:, :, :, 0], X[:, :, :, 1]
+        
+        # flip and extend beyond Nyquist frequency
+        X_real_nyquist = torch.flip(X_real, [1])
+        X_imag_nyquist = torch.flip(X_imag, [1])
+        X_real_nyquist = X_real_nyquist[:, 1:-1, :]
+        X_imag_nyquist = -X_imag_nyquist[:, 1:-1, :]
+        
+        X_real = torch.cat([X_real, X_real_nyquist], axis=1)
+        X_imag = torch.cat([X_imag, X_imag_nyquist], axis=1)
+        
+        # broadcast dimensions to support 2D convolution
+        X_real_bc = X_real.unsqueeze(1)
+        X_imag_bc = X_imag.unsqueeze(1)
+        wsin_bc = self.wsin_inv.unsqueeze(-1)
+        wcos_bc = self.wcos_inv.unsqueeze(-1)
 
-            x = padding(x)
+        a1 = conv2d(X_real_bc, wcos_bc, stride=(1,1))
+        b2 = conv2d(X_imag_bc, wsin_bc, stride=(1,1))
+
+        # compute real and imag part. signal lies in the real part
+        real = a1 - b2
+        real = real.squeeze(-2)
+        real_old = real.clone() / self.n_fft
+                
+        # each time step contains reconstructed signal, hence we stitch them together and remove
+        # the repeated segments due to overlapping windows during STFT when hop_size < n_fft
+        window_mask = get_window(self.window, self.n_fft, fftbins=True) + 1e-5
+        if is_window_normalized:
+            real /= torch.Tensor(window_mask, device=self.device).unsqueeze(0).unsqueeze(-1)
+        real /= self.n_fft
+        
+        real_last = real[:, self.stride:, -1]   # retain the signal in last time step
+        real_split = real[:, :self.stride, :]   # remove redundant samples at overlapped parts
+        
+        # reshape output signal to 1D
+        output_signal = torch.reshape(torch.transpose(real_split, 2, 1), (real_split.shape[0], -1))
+        output_signal = torch.cat([output_signal, real_last], dim=-1)    # stich the final signal back
+        
+        output_signal = output_signal[:, self.pad_amount:]
+        output_signal = output_signal[:, :self.num_samples]
+        return output_signal
+    
+    def griffin_lim(self, X, maxiter=32, tol=1e-6, alpha=0.99, verbose=True, phase=None):
+        # only use griffin lim when X is not in complex form
+        if phase is None:
+            phase = torch.rand_like(X) * 2 - 1
+            phase = phase * np.pi
+            phase[:, 0, :] = 0.0
+
+        phase = nn.Parameter(phase)
+        criterion = nn.MSELoss()
+        optimizer = optim.Adam([phase], lr=1e-2)
+        
+        for idx in range(maxiter):
+            optimizer.zero_grad()
+
+            X_real, X_imag = X * torch.cos(phase), X * torch.sin(phase)
+            X_cur = torch.stack([X_real, X_imag], dim=-1)
+            inverse_signal = self.__inverse(X_cur, is_window_normalized=False)
+
+            # Rebuild the spectrogram
+            rebuilt_mag = self.forward(inverse_signal, output_format="Complex")
+            rebuilt_mag = torch.sqrt(rebuilt_mag[:, :, :, 0].pow(2) + rebuilt_mag[:, :, :, 1].pow(2))
+            loss = criterion(rebuilt_mag, X)
+            loss.backward()
+            optimizer.step()
             
-        imag = conv1d(x, self.wsin, stride=self.stride).pow(2)
-        real = conv1d(x, self.wcos, stride=self.stride).pow(2)  # Doing STFT by using conv1d
-        return real, imag
+            print("Run: {}/{} MSE: {:.4}".format(idx + 1, maxiter, criterion(rebuilt_mag, X).item()))
+
+        # Return the final phase estimates
+        X_real, X_imag = X * torch.cos(phase), X * torch.sin(phase)
+        X_cur = torch.stack([X_real, X_imag], dim=-1)
+        return self.inverse(X_cur, is_window_normalized=False)
     
 
 class DFT(torch.nn.Module):
@@ -880,7 +961,7 @@ class MFCC(torch.nn.Module):
     def __init__(self, sr=22050, n_mfcc=20, norm='ortho', device='cuda:0', verbose=True, **kwargs):
         super(MFCC, self).__init__()
         self.melspec_layer = MelSpectrogram(sr=sr, verbose=verbose, device=device, **kwargs)
-        self.p2d = self.power_to_db()
+        self.p2d = self.power_to_db(device=device)
         self.m_mfcc = n_mfcc
         
     def forward(self, x):
@@ -1352,7 +1433,10 @@ class CQT1992v2(torch.nn.Module):
     >>> specs = spec_layer(x)
     """
 
-    def __init__(self, sr=22050, hop_length=512, fmin=32.70, fmax=None, n_bins=84, bins_per_octave=12, norm=1, window='hann', center=True, pad_mode='reflect', trainable=False, output_format='Magnitude', verbose=True, device='cuda:0'):
+    def __init__(self, sr=22050, hop_length=512, fmin=32.70, fmax=None, n_bins=84, 
+                 bins_per_octave=12, norm=1, window='hann', center=True, pad_mode='reflect', 
+                 trainable=False, output_format='Magnitude', verbose=True, device='cuda:0'):
+
         super(CQT1992v2, self).__init__()
         
         # norm arg is not functioning
