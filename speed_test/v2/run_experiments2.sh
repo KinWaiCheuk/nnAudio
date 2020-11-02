dd30426f20b309eabfb9c61b25d2c4375c948cc9
Raven Cheuk <u3500684@connect.hku.hk>
2020-11-02 16:29:08 +0800

    remove unnecessary codes

--- speed_test/v2/run_experiments2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Part 1 MAPS dataset
-# Running the experiments in GPU
-python ./MAPS/STFT.py kapre
-python ./MAPS/Melspectrogram.py kapre
-python ./MAPS/STFT.py torchaudio
-python ./MAPS/Melspectrogram.py torchaudio
