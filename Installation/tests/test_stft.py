e4b9afe76853c8072b250b1978a24c432b910bc2
Miguel Pérez <miguel_on_94@hotmail.com>
2023-03-03 22:51:59 +0100

    Update test_stft.py

--- Installation/tests/test_stft.py
+++ Installation/tests/test_stft.py
@@ -20,7 +20,7 @@ else:
     device_args = ["cpu"]
 
 # librosa example audio for testing
-example_y, example_sr = librosa.load(librosa.util.example_audio_file())
+example_y, example_sr = librosa.load(librosa.example('vibeace', hq=False))
 
 
 @pytest.mark.parametrize("n_fft, hop_length, window", stft_parameters)
